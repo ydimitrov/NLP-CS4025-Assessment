88e4c5f484a14afb33bce990fd547717588b7147
Radostin Stoyanov <rstoyanov1@gmail.com>
2017-11-18 17:10:50 +0000

    tree: Improve file read
    Handle server timeout in the output

--- src/tree.py
+++ src/tree.py
@@ -1,21 +1,23 @@
 from pycorenlp import StanfordCoreNLP
 import pprint
+
 from parseTree import parseTree
+from take_input import InputReader, INPUT_FILES
 
 pp = pprint.PrettyPrinter(indent=4)
 
 nlp = StanfordCoreNLP('http://localhost:9000')
 
-sentences = open("../Lexicons/nokia-neg.txt", "r")
-for line in sentences:
-    print line.strip()
-    text = (line)
-    output = nlp.annotate(text, properties={
+output = nlp.annotate(
+    text="\n".join(InputReader(INPUT_FILES).read())[0],  # Use only the 1st
+    properties={
       'annotators': 'tokenize,ssplit,pos,depparse,parse',
       'outputFormat': 'json'
-      })
-    # pp.pprint(output['sentences'][0])
+    }
+)
+
+if "CoreNLP request timed out" in output:
+    print("CoreNLP request timed out. Your document may be too long.")
+else:
     sampleTree = output['sentences'][0]['parse']
     print parseTree(sampleTree)
-
-    # print "\n"
