33469c5edce967c5e85acb5fea7cc8abf045e5f0
siyanapavlova <ad12spav@uwcad.it>
2017-11-18 17:21:30 +0000

    Resolving conflict

--- src/tree.py
+++ src/tree.py
@@ -9,11 +9,7 @@ pp = pprint.PrettyPrinter(indent=4)
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 output = nlp.annotate(
-<<<<<<< HEAD
-    text="\n".join(InputReader(INPUT_FILES).read())[0],  # Use only the 1st
-=======
     text=InputReader(INPUT_FILES).read()[0],  # Use only the 1st
->>>>>>> dc00abfd6cf30f3f4493c9669402ad67e04bdf66
     properties={
       'annotators': 'tokenize,ssplit,pos,depparse,parse',
       'outputFormat': 'json'
