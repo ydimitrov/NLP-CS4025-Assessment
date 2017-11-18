5f868e1e2dcb099acd2c5d93a59ef84be771b4cb
Radostin Stoyanov <rstoyanov1@gmail.com>
2017-11-18 17:40:15 +0000

    parseTree: Add annotators description

--- src/tree.py
+++ src/tree.py
@@ -8,6 +8,31 @@ pp = pprint.PrettyPrinter(indent=4)
 
 nlp = StanfordCoreNLP('http://localhost:9000')
 
+# Available Annotators:
+#     - tokenize: Splits the text into roughly “words”.
+#     - cleanxml: Remove xml tokens.
+#     - ssplit: Splits a sequence of tokens into sentences.
+#     - pos: Labels tokens with their POS tag.
+#     - lemma: Generates the word lemmas for all tokens in the corpus.
+#     - ner: Recognizes named (PERSON, LOCATION, ORGANIZATION, MISC),
+#           numerical (MONEY, NUMBER, ORDINAL, PERCENT), and temporal
+#           (DATE, TIME, DURATION, SET) entities.
+#     - regexner: Rule-based NER over token sequences using Java regular
+#           expressions.
+#     - sentiment: Socher et al’s sentiment model. Attaches a binarized tree of
+#           the sentence to the sentence level CoreMap.
+#     - truecase: Recognizes the true case of tokens in text where this
+#           information was lost, e.g., all upper case text.
+#     - parse: Full syntactic analysis, using both the constituent and the
+#           dependency representations.
+#     - depparse: Syntactic dependency parser.
+#     - dcoref: Pronominal and nominal coreference resolution.
+#     - relation: Relation extractor find relations between two entities.
+#     - natlog: Marks quantifier scope and token polarity, according to
+#           natural logic semantics.
+#     - quote: Deterministically picks out quotes delimited
+#           by " or ' from a text.
+
 output = nlp.annotate(
     text=InputReader(INPUT_FILES).read()[0],  # Use only the 1st
     properties={
