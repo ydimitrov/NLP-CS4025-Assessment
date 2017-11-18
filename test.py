9e5c521923371170c1009e0bfc4a6e65193c91be
siyanapavlova <ad12spav@uwcad.it>
2017-11-18 00:19:05 +0000

    Testing test

--- test.py
+++ test.py
@@ -5,14 +5,27 @@ pp = pprint.PrettyPrinter(indent=4)
 
 nlp = StanfordCoreNLP('http://localhost:9000')
 
-text = (
-  'Pusheen and Smitha walked along the beach. '
-  'Pusheen wanted to surf, but fell off the surfboard.')
-output = nlp.annotate(text, properties={
-  'annotators': 'tokenize,ssplit,pos,depparse,parse',
-  'outputFormat': 'json'
-  })
+sentences = open("nokia-neg.txt", "r")
+for line in sentences:
+    print line.strip()
+    text = (line)
+    output = nlp.annotate(text, properties={
+      'annotators': 'tokenize,ssplit,pos,depparse,parse',
+      'outputFormat': 'json'
+      })
+    # print(output['sentences'][0]['parse'])
+    pp.pprint(output['sentences'])
 
+    # print "\n"
 
-print(output['sentences'][0]['parse'])
-pp.pprint(output)
+# text = (
+#   'Pusheen and Smitha walked along the beach. '
+#   'Pusheen wanted to surf, but fell off the surfboard.')
+# output = nlp.annotate(text, properties={
+#   'annotators': 'tokenize,ssplit,pos,depparse,parse',
+#   'outputFormat': 'json'
+#   })
+#
+#
+# print(output['sentences'][0]['parse'])
+# pp.pprint(output)
